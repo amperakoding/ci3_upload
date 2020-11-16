25bfc68e001b7b1f5daeb20094759baf0e8c6073
AmperaKoding <amperakoding@gmail.com>
2020-11-16 08:14:23 +0700

    atur halaman welcome

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Selamat Datang di CI3_Upload</title>
 
 	<style type="text/css">
 
@@ -27,8 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	h1 {
 		color: #444;
 		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
+		border-bottom: 1px solid #D0D0D0;		
 		font-weight: normal;
 		margin: 0 0 14px 0;
 		padding: 14px 15px 10px 15px;
@@ -68,18 +67,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Selamat Datang di CI3_Upload</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>Project ini berisi tentang bagaimana cara upload file menggunakan Codeigniter 3 (3.1.11) dan MySQL.</p>
+		<p>Semoga bermanfaat.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>
+			Anda dapat mengakses contoh upload yang sudah ada sebagai berikut:
+			<ul>
+				<li>Upload file super sederhana, <a href="<?php echo base_url('buku') ?>">klik disini</a>.</li>
+				<li>Upload file dengan thumbnail, <a href="<?php echo base_url('berita') ?>">klik disini</a>.</li>
+			</ul>
+		</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
